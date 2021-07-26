08611733cd13d1295c883baca4296fb92f76bfc5
José Eduardo Rodrigues Pinto Jerp86 <jerp4@hotmail.com>
2021-07-26 16:01:34 -0300

    add Wrapper, Logo & FutureMap styled

--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,5 +1,9 @@
 import styled from 'styled-components';
 
+export const Wrapper = styled.div`
+  display: flex;
+`;
+
 export const Container = styled.aside`
   background-color: ${({ theme }) => theme.colors.background};
   width: 360px;
@@ -14,4 +18,13 @@ export const Search = styled.section`
 
   background-color: ${({ theme }) => theme.colors.white};
   padding: 16px;
+`;
+
+export const Logo = styled.img`
+  margin-bottom: 16px;
+`;
+
+export const FutureMap = styled.div`
+  flex: 1;
+  background-color: red;
 `;
