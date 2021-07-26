0495329b5d7b29bf39a19d551f4c2594b5e240f5
José Eduardo Rodrigues Pinto Jerp86 <jerp4@hotmail.com>
2021-07-26 16:37:59 -0300

    add Slider in Home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,13 +1,27 @@
 import React, { useState } from 'react';
 import TextField, { Input } from '@material/react-text-field';
 import MaterialIcon from '@material/react-material-icon';
+import Slider from 'react-slick';
 
 import logo from '../../assets/logo.svg';
+import restaurant from '../../assets/restaurante-fake.png';
+import restaurant2 from '../../assets/restaurante-fake2.jpg';
+import restaurant3 from '../../assets/restaurante-fake3.jpg';
+import restaurant4 from '../../assets/restaurante-fake4.jpg';
 
 import { CarouselTitle, Container, FutureMap, Logo, Search, Wrapper } from './styles';
 
 const Home = () => {
   const [inputValue, setInputValue] = useState('');
+
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    adaptiveHeight: true,
+  };
 
   return (
     <Wrapper>
@@ -23,6 +37,32 @@ const Home = () => {
           </TextField>
 
           <CarouselTitle>Na sua Área</CarouselTitle>
+          <Slider {...settings}>
+            <div>
+              <img src={restaurant} alt="Fake Restaurant 1" />
+            </div>
+            <div>
+              <img src={restaurant2} alt="Fake Restaurant 2" />
+            </div>
+            <div>
+              <img src={restaurant3} alt="Fake Restaurant 3" />
+            </div>
+            <div>
+              <img src={restaurant4} alt="Fake Restaurant 4" />
+            </div>
+            <div>
+              <img src={restaurant} alt="Fake Restaurant 5" />
+            </div>
+            <div>
+              <img src={restaurant3} alt="Fake Restaurant 6" />
+            </div>
+            <div>
+              <img src={restaurant2} alt="Fake Restaurant 7" />
+            </div>
+            <div>
+              <img src={restaurant4} alt="Fake Restaurant 8" />
+            </div>
+          </Slider>
         </Search>
       </Container>
 
