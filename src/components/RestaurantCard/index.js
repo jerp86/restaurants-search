4a5247569a1206f63eb6ec83a10e9e8110eb51b4
José Eduardo Rodrigues Pinto Jerp86 <jerp4@hotmail.com>
2021-07-27 06:12:04 -0300

    add react-rating-stars-component in RestaurantCard

--- src/components/RestaurantCard/index.js
+++ src/components/RestaurantCard/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactStars from 'react-rating-stars-component';
 
 import { Address, Restaurant, RestaurantInfo, Title } from './styles';
 
@@ -6,7 +7,7 @@ const RestaurantCard = () => (
   <Restaurant>
     <RestaurantInfo>
       <Title>Nome do Restaurante</Title>
-      <p>Avaliação</p>
+      <ReactStars count={5} isHalf value={3.5} edit={false} activeColor="#E7711C" />
       <Address>Avenida Universitária, 2376</Address>
     </RestaurantInfo>
   </Restaurant>
