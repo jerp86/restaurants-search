5bba6c5e6008280ef3d71f2a0539e9d0439a030c
José Eduardo Rodrigues Pinto Jerp86 <jerp4@hotmail.com>
2021-07-28 06:54:04 -0300

    add functions in the Map component for various types of search

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -1,10 +1,56 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { GoogleApiWrapper, Map, Marker } from 'google-maps-react';
 
-export const MapContainer = ({ google }) => {
+export const MapContainer = ({ google, query }) => {
   const [map, setMap] = useState(null);
 
-  return <Map google={google} centerAroundCurrentLocation />;
+  function searchByQuery(query) {
+    const service = new google.maps.places.PlacesService(map);
+
+    const request = {
+      location: map.center,
+      radius: '200',
+      type: ['restaurant'],
+      query,
+    };
+
+    service.textSearch(request, (results, status) => {
+      if (status === google.maps.places.PlacesServiceStatus.OK) {
+        console.log('QUERY>>>', results);
+      }
+    });
+  }
+
+  function searchNearby(map, center) {
+    const service = new google.maps.places.PlacesService(map);
+
+    const request = {
+      location: center,
+      radius: '20000',
+      type: ['restaurant'],
+    };
+
+    service.nearbySearch(request, (results, status) => {
+      if (status === google.maps.places.PlacesServiceStatus.OK) {
+        console.log('restaurants>>>', results);
+      }
+    });
+  }
+
+  function onMapReady(_, map) {
+    setMap(map);
+    searchNearby(map, map.center);
+  }
+
+  useEffect(() => {
+    if (query) {
+      searchByQuery(query);
+    }
+  }, [query]);
+
+  return (
+    <Map google={google} centerAroundCurrentLocation onReady={onMapReady} onRecenter={onMapReady} />
+  );
 };
 
 export default GoogleApiWrapper({
