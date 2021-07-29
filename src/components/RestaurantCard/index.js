c05181f3a1e49115291b7e03a0cfb603087ff082
José Eduardo Rodrigues Pinto Jerp86 <jerp4@hotmail.com>
2021-07-29 06:54:55 -0300

    add restaurant properties to RestaurantCard component

--- src/components/RestaurantCard/index.js
+++ src/components/RestaurantCard/index.js
@@ -3,18 +3,27 @@ import ReactStars from 'react-rating-stars-component';
 
 import { Address, Restaurant, RestaurantInfo, RestaurantPhoto, Title } from './styles';
 
-import restaurant from '../../assets/restaurante-fake.png';
+import fakeRestaurant from '../../assets/restaurante-fake.png';
 import theme from '../../theme';
 
-const RestaurantCard = () => (
+const RestaurantCard = ({ restaurant }) => (
   <Restaurant>
     <RestaurantInfo>
-      <Title>Nome do Restaurante</Title>
-      <ReactStars count={5} isHalf value={3.5} edit={false} activeColor={theme.colors.star} />
-      <Address>Avenida Universitária, 2376</Address>
+      <Title>{restaurant.name}</Title>
+      <ReactStars
+        count={5}
+        isHalf
+        value={restaurant.rating}
+        edit={false}
+        activeColor={theme.colors.star}
+      />
+      <Address>{restaurant.vicinity || restaurant.formatted_address}</Address>
     </RestaurantInfo>
 
-    <RestaurantPhoto src={restaurant} alt="Foto do restaurante" />
+    <RestaurantPhoto
+      src={restaurant.photos ? restaurant.photos[0].getUrl() : fakeRestaurant}
+      alt={`Foto de ${restaurant.name}`}
+    />
   </Restaurant>
 );
 
