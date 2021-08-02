779e020f9a4c74a57ce4ac701f09142e35f7759f
José Eduardo Rodrigues Pinto Jerp86 <jerp4@hotmail.com>
2021-08-02 06:47:19 -0300

    refactory: add skeleton in RestaurantCard

--- src/components/RestaurantCard/index.js
+++ src/components/RestaurantCard/index.js
@@ -1,30 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactStars from 'react-rating-stars-component';
 
+import Skeleton from '../Skeleton';
 import { Address, Restaurant, RestaurantInfo, RestaurantPhoto, Title } from './styles';
 
 import fakeRestaurant from '../../assets/restaurante-fake.png';
 import theme from '../../theme';
 
-const RestaurantCard = ({ restaurant, onClick }) => (
-  <Restaurant onClick={onClick}>
-    <RestaurantInfo>
-      <Title>{restaurant.name}</Title>
-      <ReactStars
-        count={5}
-        isHalf
-        value={restaurant.rating}
-        edit={false}
-        activeColor={theme.colors.star}
-      />
-      <Address>{restaurant.vicinity || restaurant.formatted_address}</Address>
-    </RestaurantInfo>
+const RestaurantCard = ({ restaurant, onClick }) => {
+  const [imageLoaded, setImageLoaded] = useState(false);
 
-    <RestaurantPhoto
-      src={restaurant.photos ? restaurant.photos[0].getUrl() : fakeRestaurant}
-      alt={`Foto de ${restaurant.name}`}
-    />
-  </Restaurant>
-);
+  return (
+    <Restaurant onClick={onClick}>
+      <RestaurantInfo>
+        <Title>{restaurant.name}</Title>
+        <ReactStars
+          count={5}
+          isHalf
+          value={restaurant.rating}
+          edit={false}
+          activeColor={theme.colors.star}
+        />
+        <Address>{restaurant.vicinity || restaurant.formatted_address}</Address>
+      </RestaurantInfo>
+
+      <RestaurantPhoto
+        src={restaurant.photos ? restaurant.photos[0].getUrl() : fakeRestaurant}
+        alt={`Foto de ${restaurant.name}`}
+        onLoad={() => setImageLoaded(true)}
+        imageLoaded={imageLoaded}
+      />
+
+      {!imageLoaded && <Skeleton width="100px" height="100px" />}
+    </Restaurant>
+  );
+};
 
 export default RestaurantCard;
